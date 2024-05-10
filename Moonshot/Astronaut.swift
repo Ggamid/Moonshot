9ef54ff6c95fbd25e06ac42c2a943700dd5f517d
Гамид Халидов <khalidov-gamid@mail.ru>
2024-05-10 19:16:00 +0300

    Добавил контент в Assets, создал расширение для класса Bundle, которое позволяет декодировать astronaut.json

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Moonshot
+//
+//  Created by Gamıd Khalıdov on 10.05.2024.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Mission.swift
@@ -0,0 +1,21 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Gamıd Khalıdov on 10.05.2024.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable{
+    
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let descreption: String
+}
